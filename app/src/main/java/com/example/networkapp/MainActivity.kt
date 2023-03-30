71a6ebc19fa633ee149cca31865e5f6cc21fad0d
Francis R Scallatino <tup31915@temple.edu>
2023-03-29 22:36:17 -0400

    update the TextView to show output from URL stream

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.networkapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textDisplay = findViewById<TextView>(R.id.textDisplay)
 
         //the IO dispatcher is used for input/output operations
         //e.g. network access
@@ -24,12 +28,15 @@ class MainActivity : AppCompatActivity() {
                 val strBuilder = StringBuilder()
 
                 while(readLine().let {
-                        strBuilder.append(it)
+                        strBuilder.append("$it\n")
                         it != null
                     });
                 strBuilder.toString()
             }
             Log.d("Response", response)
+            withContext(Dispatchers.Main){
+                textDisplay.text = response
+            }
         }
 
     }
